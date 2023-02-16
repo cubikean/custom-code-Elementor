625b5f67ca383f465626cb0e7361222d46fcbf5e
cubikean <hugochatigny@gmail.com>
2023-02-16 16:01:36 +0100

    V1.1, Ajout de plusieurs images et texte (Repeater dans un Repeater semble complex voir ne fonctionne pas)

--- widget_fonctionnel/widgets/tab-content/tab-content.php
+++ widget_fonctionnel/widgets/tab-content/tab-content.php
@@ -76,6 +76,7 @@ class Tab_Content extends Widget_Base
 			]
 		);
 
+
 		$repeater = new \Elementor\Repeater();
 
 		$repeater->add_control(
@@ -83,9 +84,7 @@ class Tab_Content extends Widget_Base
 			[
 				'label' => __('Icon de l\'onglet', 'tab-content'),
 				'type' => \Elementor\Controls_Manager::MEDIA,
-				'default' => [
-					'url' => \Elementor\Utils::get_placeholder_image_src(),
-				],
+
 			]
 		);
 
@@ -134,19 +133,90 @@ class Tab_Content extends Widget_Base
 				'dynamic' => [
 					'active' => true,
 				],
-				'default' => [
-					'url' => \Elementor\Utils::get_placeholder_image_src(),
+
+			]
+		);
+
+		$repeater->add_control(
+			'Icon1',
+			[
+				'label' => __('Icon 1', 'tab-content'),
+				'type' => \Elementor\Controls_Manager::MEDIA,
+
+			]
+		);
+
+		$repeater->add_control(
+			'Icon1Title',
+			[
+				'label' => __('Titre 1er icon', 'tab-content'),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'input_type' => 'text',
+				'dynamic' => [
+					'active' => true,
 				],
 			]
 		);
 
 		$repeater->add_control(
-			'GalleryIcon',
+			'Icon2',
 			[
-				'label' => __('Galerie icon', 'tab-content'),
-				'type' => \Elementor\Controls_Manager::GALLERY,
-				'default' => [
-					'url' => \Elementor\Utils::get_placeholder_image_src(),
+				'label' => __('Icon 2', 'tab-content'),
+				'type' => \Elementor\Controls_Manager::MEDIA,
+
+			]
+		);
+
+		$repeater->add_control(
+			'Icon2Title',
+			[
+				'label' => __('Titre 2er icon', 'tab-content'),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'input_type' => 'text',
+				'dynamic' => [
+					'active' => true,
+				],
+			]
+		);
+
+		$repeater->add_control(
+			'Icon3',
+			[
+				'label' => __('Icon 3', 'tab-content'),
+				'type' => \Elementor\Controls_Manager::MEDIA,
+
+			]
+		);
+
+		$repeater->add_control(
+			'Icon3Title',
+			[
+				'label' => __('Titre 3er icon', 'tab-content'),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'input_type' => 'text',
+				'dynamic' => [
+					'active' => true,
+				],
+			]
+		);
+
+		$repeater->add_control(
+			'Icon4',
+			[
+				'label' => __('Icon 4', 'tab-content'),
+				'type' => \Elementor\Controls_Manager::MEDIA,
+
+			]
+		);
+
+		$repeater->add_control(
+			'Icon4Title',
+			[
+				'label' => __('Titre 4er icon', 'tab-content'),
+				'type' => \Elementor\Controls_Manager::TEXT,
+				'input_type' => 'text',
+				'dynamic' => [
+					'active' => true,
 				],
 			]
 		);
@@ -162,19 +232,13 @@ class Tab_Content extends Widget_Base
 				'default' => [
 					[
 						'TitleTab' => esc_html__('Titre de l\'onglet ...', 'tab-content'),
-						// 'ImageTab' => esc_html__( 'Image de l\'onglet', 'tab-content' ),
 						'TitleContent' => esc_html__('Titre de votre contenu...', 'tab-content'),
 						'Content' => esc_html__('Votre contenu...', 'tab-content'),
-						// 'ImageContent' => esc_html__( 'Image de votre contenu.', 'tab-content' ),
-						// 'GalleryIcon' => esc_html__( 'Icon bas de page.', 'tab-content' ),
 					],
 					[
 						'TitleTab' => esc_html__('Titre de l\'onglet ...', 'tab-content'),
-						// 'ImageTab' => esc_html__( 'Image de l\'onglet', 'tab-content' ),
 						'TitleContent' => esc_html__('Titre de votre contenu...', 'tab-content'),
 						'Content' => esc_html__('Votre contenu...', 'tab-content'),
-						// 'ImageContent' => esc_html__( 'Image de votre contenu.', 'tab-content' ),
-						// 'GalleryIcon' => esc_html__( 'Icon bas de page.', 'tab-content' ),
 					],
 				],
 				'title_field' => '{{{ TitleTab }}}',
@@ -210,7 +274,7 @@ class Tab_Content extends Widget_Base
 				<input checked="checked" id="tab1" type="radio" name="pct" />
 				<?php foreach ($settings['list'] as $item) { ?>
 					<input id="tab<?= $tabId ?>" type="radio" name="pct" />
-					<?php $tabId = $tabId + 1;
+			<?php $tabId = $tabId + 1;
 				}
 			} ?>
 			<nav>
@@ -218,17 +282,18 @@ class Tab_Content extends Widget_Base
 
 					<?php if ($settings['list']) {
 						$valueIncRender = 1; ?>
-						<?php foreach ($settings['list'] as $index => $item) { 
+						<?php foreach ($settings['list'] as $index => $item) {
 
 							// inline editing setup for TitleTab
 							$repeater_setting_key_title_tab = $this->get_repeater_setting_key('TitleTab', 'list', $index);
 							$this->add_inline_editing_attributes($repeater_setting_key_title_tab);
 
-							?>
+						?>
 							<li class="tab<?= $valueIncRender ?>">
 								<label for="tab<?= $valueIncRender ?>" class="image-icon">
 									<img src="<?= $item['ImageTab']['url'] ?>" alt="" />
-									<p <?php $this->print_render_attribute_string($repeater_setting_key_title_tab) ?>><?php $title_tab = $settings['list'][$index]['TitleTab']; echo $title_tab ?> </p>
+									<p <?php $this->print_render_attribute_string($repeater_setting_key_title_tab) ?>><?php $title_tab = $settings['list'][$index]['TitleTab'];
+																														echo $title_tab ?> </p>
 								</label>
 							</li>
 					<?php $valueIncRender = $valueIncRender + 1;
@@ -241,7 +306,7 @@ class Tab_Content extends Widget_Base
 
 				<?php if ($settings['list']) {
 					$valueIncRenderTab = 1; ?>
-					<?php foreach ($settings['list'] as $index => $item) { 
+					<?php foreach ($settings['list'] as $index => $item) {
 
 						// inline editing setup for TitleContent
 						$repeater_setting_key_title_content = $this->get_repeater_setting_key('TitleContent', 'list', $index);
@@ -251,7 +316,7 @@ class Tab_Content extends Widget_Base
 						$repeater_setting_key_content = $this->get_repeater_setting_key('Content', 'list', $index);
 						$this->add_inline_editing_attributes($repeater_setting_key_content);
 
-						?>
+					?>
 						<div class="tab tab<?= $valueIncRenderTab ?>">
 							<div class="tab-container">
 								<div class="tab-container_image">
@@ -260,35 +325,46 @@ class Tab_Content extends Widget_Base
 								<div class="tab-container_content">
 									<h2 <?php $this->print_render_attribute_string($repeater_setting_key_title_content) ?> class="tab-container_content_title">
 
-										<?php $title = $settings['list'][$index]['TitleContent']; echo $title ?>
+										<?php $title = $settings['list'][$index]['TitleContent'];
+										echo $title ?>
 
 									</h2>
 									<p <?php $this->print_render_attribute_string($repeater_setting_key_content) ?> class="tab-container_content_text">
 
-										<?php $content = $settings['list'][$index]['Content'];echo $content ?>
+										<?php $content = $settings['list'][$index]['Content'];
+										echo $content ?>
 
 									</p>
 								</div>
 								<div class="tab-container_icon">
-
-									<?php if ($item['GalleryIcon']) {
-										foreach ($item['GalleryIcon'] as $image) { 
-
-											$image_id = $image['id'];
-											$image_data = wp_prepare_attachment_for_js($image_id);
-											$image_caption = $image_data['caption'];
-											?>
-
-											<?php $alt = get_post_meta($image['id'], '_wp_attachment_image_alt', true); ?>
-											<div class="tab-container_icon_image"><img src="<?php echo $image['url'] ?>" alt="<?php echo $alt ?>" class="icon" />
-												<p><?php echo esc_html($image_caption) ?></p>
-											</div>
-									<?php }
-									} ?>
+									<?php if (!empty($item['Icon1']['url'])) { ?>
+										<div class="tab-container_icon_image">
+											<img src="<?php echo $item['Icon1']['url'] ?>" alt="" class="icon" />
+											<p><?php echo $item['Icon1Title'] ?></p>
+										</div>
+									<?php } ?>
+									<?php if (!empty($item['Icon2']['url'])) { ?>
+										<div class="tab-container_icon_image">
+											<img src="<?php echo $item['Icon2']['url'] ?>" alt="" class="icon" />
+											<p><?php echo $item['Icon2Title'] ?></p>
+										</div>
+									<?php } ?>
+									<?php if (!empty($item['Icon3']['url'])) { ?>
+										<div class="tab-container_icon_image">
+											<img src="<?php echo $item['Icon3']['url'] ?>" alt="" class="icon" />
+											<p><?php echo $item['Icon3Title'] ?></p>
+										</div>
+									<?php } ?>
+									<?php if (!empty($item['Icon4']['url'])) { ?>
+										<div class="tab-container_icon_image">
+											<img src="<?php echo $item['Icon4']['url'] ?>" alt="" class="icon" />
+											<p><?php echo $item['Icon4Title'] ?></p>
+										</div>
+									<?php } ?>
 								</div>
 							</div>
 						</div>
-					<?php $valueIncRenderTab = $valueIncRenderTab + 1;
+				<?php $valueIncRenderTab = $valueIncRenderTab + 1;
 					}
 				} ?>
 
@@ -311,58 +387,58 @@ class Tab_Content extends Widget_Base
 							<input checked="checked" id="tab1" type="radio" name="pct" />
 							<# _.each( settings.list, function( item ) { #>
 								<input id="tab{{ tabId }}" type="radio" name="pct" />
-							<# tabId=tabId + 1; #>
-						<# }); #>
-					<# } #>
-					<nav>
-						<ul>
-							<# if ( settings.list ) { #>
-								<# var valueIncTemplate=1; #>
-								<# _.each( settings.list, function( item ) { #>
-									<li class="tab{{ valueIncTemplate }}">
-										<label for="tab{{ valueIncTemplate }}" class="image-icon">
-											<img src="{{ item.ImageTab.url }}" alt="" />
-											<p>{{{ item.TitleTab }}}</p>
-										</label>
-									</li>
-								<# valueIncTemplate=valueIncTemplate + 1; #>
-								<# }); #>
-							<# } #>
+								<# tabId=tabId + 1; #>
+									<# }); #>
+										<# } #>
+											<nav>
+												<ul>
+													<# if ( settings.list ) { #>
+														<# var valueIncTemplate=1; #>
+															<# _.each( settings.list, function( item ) { #>
+																<li class="tab{{ valueIncTemplate }}">
+																	<label for="tab{{ valueIncTemplate }}" class="image-icon">
+																		<img src="{{ item.ImageTab.url }}" alt="" />
+																		<p>{{{ item.TitleTab }}}</p>
+																	</label>
+																</li>
+																<# valueIncTemplate=valueIncTemplate + 1; #>
+																	<# }); #>
+																		<# } #>
 
-						</ul>
-					</nav>
+												</ul>
+											</nav>
 
-					<section>
-						<# if ( settings.list ) { #>
-							<# var valueIncTemplateSection=1 ;#>
-								<# _.each( settings.list, function( item ) { #>
+											<section>
+												<# if ( settings.list ) { #>
+													<# var valueIncTemplateSection=1 ;#>
+														<# _.each( settings.list, function( item ) { #>
 
-										<div class="tab tab{{ valueIncTemplateSection }}">
-											<div class="tab-container">
-												<div class="tab-container_image">
-													<img src="{{ item.ImageContent.url }}" alt="" />
-												</div>
-												<div class="tab-container_content">
-													<h2 class="tab-container_content_title">
-														{{{ item.TitleContent }}}
-													</h2>
-													<p class="tab-container_content_text">
-														{{{ item.Content }}}
-													</p>
-												</div>
-												<div class="tab-container_icon">
-													<# if ( item.GalleryIcon ) { #>
-														<# _.each( item.GalleryIcon, function( image ) { #>
-															<div class="tab-container_icon_image"><img src="{{ image.url }}" alt="" class="icon" /></div>
-														<# }); #>
-													<# } #>
-												</div>
-											</div>
-										</div>
-								<# valueIncTemplateSection=valueIncTemplateSection + 1 ;#>
-								<# }); #>
-							<# } #>
-					</section>
+															<div class="tab tab{{ valueIncTemplateSection }}">
+																<div class="tab-container">
+																	<div class="tab-container_image">
+																		<img src="{{ item.ImageContent.url }}" alt="" />
+																	</div>
+																	<div class="tab-container_content">
+																		<h2 class="tab-container_content_title">
+																			{{{ item.TitleContent }}}
+																		</h2>
+																		<p class="tab-container_content_text">
+																			{{{ item.Content }}}
+																		</p>
+																	</div>
+																	<div class="tab-container_icon">
+																		<# if ( item.GalleryIcon ) { #>
+																			<# _.each( item.GalleryIcon, function( image ) { #>
+																				<div class="tab-container_icon_image"><img src="{{ image.url }}" alt="" class="icon" /></div>
+																				<# }); #>
+																					<# } #>
+																	</div>
+																</div>
+															</div>
+															<# valueIncTemplateSection=valueIncTemplateSection + 1 ;#>
+																<# }); #>
+																	<# } #>
+											</section>
 				</div>
 
 
