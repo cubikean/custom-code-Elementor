1fef8147178b81b2650734d99f2e283e0f083819
cubikean <hugochatigny@gmail.com>
2023-06-23 09:09:33 +0200

    messgae

---
+++ scriptPython/compress_video.py
@@ -0,0 +1,19 @@
+from moviepy.editor import VideoFileClip
+
+def compress_video(input_path, output_path, target_bitrate):
+    video = VideoFileClip(input_path)
+    
+    # Compress the video by applying the specified target bitrate (in kbps)
+    compressed_video = video.resize(width=video.w // 2)  # Reduce the video size
+    
+    # Save the compressed video with target bitrate
+    compressed_video.write_videofile(output_path, codec="libx264", audio_codec="aac", bitrate=target_bitrate)
+    
+    print("The video has been compressed successfully!")
+
+# Example usage
+input_path = "C:/Users/Beekom/Desktop/dev/DEV/scriptPython/videoInput/CDC.mp4"
+output_path = "C:/Users/Beekom/Desktop/dev/DEV/scriptPython/videoOutput/video.mp4"
+target_bitrate = "1000k"  # Target bitrate for compression in kbps
+
+compress_video(input_path, output_path, target_bitrate)
